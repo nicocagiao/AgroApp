6a35f1ccd8192e0645f7899ff136267e566ce00a
Nico <nicolascagiao@hotmail.com>
2021-02-20 13:20:47 -0300

    :tada: initial commit

---
+++ client/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+     <div class="jumbotron">
+    <h1 class="display-3">AgroAPP</h1>
+    <p class="lead">What?</p>
+    <hr class="my-4">
+    <p>Container</p>
+    <p class="lead">
+    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+  </p>
+</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+};
+</script>
